529dcc7f79d03c73fe47ecdfbd394a0507c92a3c
Thomah <thomas@oxynum.fr>
2014-11-08 22:14:02 +0100

    add database config

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141108174028) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: true do |t|
     t.string   "authentication_token"
     t.string   "code",                 null: false
